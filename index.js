659571a516590e0b87d2bea85da1d2d6e0ee6a45
Jacob Bryan <jtb36688@gmail.com>
2019-02-27 18:29:50 -0500

    Added login, started register route

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWTSECRET || "100wordbackupjwtsecret"
 
-const db = require("./data/user-model.js");
+const db = require("./users/users-model.js");
 const dbConfig = require("./data/dbConfig.js")
 
 const server = express();
@@ -10,3 +12,30 @@ server.use(express.json());
 server.use(cors({ credentials: true, origin: true }));
 server.use("/api/restricted", restricted);
 server.use(session(sessionConfig));
+
+const makejwt = (user) => {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: []
+    }
+    const options = {
+        expiresIn: "8h"
+    }
+    return jwt.sign(payload, options)
+}
+
+server.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+    db.findBy({ username })
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = makejwt(user);
+            res.status(200).json({message: `welcome ${user.username}`,
+        token})
+        } else {
+            res.status(401).json({ message: "You shall not pass!" });
+        }
+    })
+})
